ea11ac6435ed4424ed9a2e22420212dec164de92
Peter Magerøy <petermageroy@gmail.com>
2024-04-09 14:50:18 +0200

    Genius peter adding test

--- IrisClassification.py
+++ IrisClassification.py
@@ -21,20 +21,28 @@ virginica_training = iris.data[100:130]
 virginica_test = iris.data[130:150]
 
 training_features = np.concatenate((setosa_training, versicolor_training, virginica_training))
+test_features = np.concatenate((setosa_test, versicolor_test, virginica_test))
 
 
 # Define labels for training
-total_samples = len(training_features)
-samples_per_class = total_samples // C
+total_training_samples = len(training_features)
+total_test_samples = len(test_features)
+
+training_samples = total_training_samples // C
+test_samples = total_test_samples // C
 
 labels_training = []
+labels_training.extend([[1, 0, 0]] * training_samples)
+labels_training.extend([[0, 1, 0]] * training_samples)
+labels_training.extend([[0, 0, 1]] * training_samples)
+labels_training = np.array(labels_training)
 
-labels_training.extend([[1, 0, 0]] * samples_per_class)
-labels_training.extend([[0, 1, 0]] * samples_per_class)
-labels_training.extend([[0, 0, 1]] * samples_per_class)
-
-labels = np.array(labels_training)
-
+labels_test = []
+labels_test.extend([0] * test_samples)
+labels_test.extend([1] * test_samples)
+labels_test.extend([2] * test_samples)
+labels_test = np.array(labels_test)
+print(labels_test)
 
 
 # Initialize weights and bias
@@ -54,6 +62,8 @@ def compute_gradient(X, y, y_pred):
     grad_wo = -2 * np.mean(y - y_pred)
     return grad_W, grad_wo
 
+
+######## Training ########
 
 # Training parameters
 learning_rate = 0.01
@@ -77,6 +87,29 @@ for epoch in range(epochs):
 
 
 
+######## Testing ########
+total_classifications = len(test_features)
+total_correct = 0
+total_false = 0
+
+# Iterate through the tests
+for i in range(len(test_features)):
+    g = np.dot(test_features[i], W.T) + w_o
+    #print("True class:\t", labels_test[i], "\tClassification:\t", np.argmax(g))
+
+    # Count correct and false classifications
+    if labels_test[i] == np.argmax(g):
+        total_correct += 1
+    else:
+        total_false += 1
+
+# In case we count wrong
+if total_correct + total_false != total_classifications:
+    print("Error! Number of total classifications incorrect.")
+
+print("Correct classifications: ", np.round(total_correct/total_classifications, 2), "%\tFalse classifications: ", np.round(total_false/total_classifications, 2), "%")
+
+
 """
 _, ax = plt.subplots()
 scatter = ax.scatter(iris.data[:, 2], iris.data[:, 3], c=iris.target)
@@ -84,6 +117,6 @@ ax.set(xlabel=iris.feature_names[2], ylabel=iris.feature_names[3])
 _ = ax.legend(
     scatter.legend_elements()[0], iris.target_names, loc="lower right", title="Classes"
 )
-
-plt.savefig("ClassesPetalScatterplot.png")
+plt.show()
+#plt.savefig("ClassesPetalScatterplot.png")
 """
